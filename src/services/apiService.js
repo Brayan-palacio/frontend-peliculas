78cb993a02980eff1960a454e8dc4524b901d2a3
Brayan palacio <20brayanpalacio@gmail.com>
2026-09-21 15:17:10 -0500

    Actualizar baseURL a Render

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api', 
+  baseURL: 'https://api-peliculas-dfhd.onrender.com/api', 
   headers: {
     'Content-Type': 'application/json'
   }
